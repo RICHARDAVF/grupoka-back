03ddb64adfc33ba08aa1b30626cc845435e44fc2
RICHARDAVF <richarferro1308@gmail.com>
2024-07-30 14:07:49 -0500

    se integra react en django

--- api_grupo_ka/urls.py
+++ api_grupo_ka/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from api_grupo_ka.view.orden_trabajo.views import ListOT,StateView
 from api_grupo_ka.view.login.views import LoginView
+from django.contrib.staticfiles.urls import static
+from django.conf import settings
+
 urlpatterns = [
     path("login/",LoginView.as_view(),name="login"),
     path("list/ot/",ListOT.as_view(),name="list_ot"),
     path("state/ot/",StateView.as_view(),name="state_ot")
 ]
+urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
