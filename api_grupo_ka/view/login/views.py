988ce4f38f95f49186b8da1f698f61ec74f98df4
RICHARDAVF <richarferro1308@gmail.com>
2024-09-06 08:59:09 -0500

    add files

--- api_grupo_ka/view/login/views.py
+++ api_grupo_ka/view/login/views.py
@@ -4,8 +4,6 @@ from api_grupo_ka.conexion import CAQ
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 class LoginView(GenericAPIView):
-
-
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
